453565f48e2edebdbb066643cf2ceeb72c7cf279
Pierre Marchand <pierremarc07@gmail.com>
2017-10-12 16:16:24 +0200

    [metadata] back to list button

--- metadata/src/components/single/index.ts
+++ metadata/src/components/single/index.ts
@@ -3,11 +3,16 @@ import tr from '../../locale';
 import { getMdTitle, getMdDescription, getCurrentDatasetMetadata } from '../../queries/metadata';
 import { setMdTitle, setMdDescription } from '../../events/metadata';
 import { Inspire, MessageRecord } from 'sdi/source';
+import button from '../button';
+import appEvents from '../../events/app';
+import { AppLayout } from '../../shape';
 
 export interface MdForm {
     title: MessageRecord;
     description: MessageRecord;
 }
+
+const toListButton = button('navigate', 'sheetList');
 
 const defaultMessage = () => ({ fr: '', nl: '' });
 
@@ -54,48 +59,69 @@ const renderTextArea =
             })
         );
     };
-const renderForm =
+
+
+
+
+
+const renderEdit =
+    (m: Inspire) => (DIV({ className: 'meta-edit' },
+        DIV({ className: 'app-col-wrapper meta-fr' },
+            DIV({ className: 'app-col-header' }, 'FR'),
+            DIV({ className: 'app-col-main' },
+                renderInputText('Titre',
+                    getMdTitle('fr'), setMdTitle('fr')),
+                renderTextArea('Résumé',
+                    getMdDescription('fr'), setMdDescription('fr')))),
+        DIV({ className: 'app-col-wrapper meta-nl' },
+            DIV({ className: 'app-col-header' }, 'NL'),
+            DIV({ className: 'app-col-main' },
+                renderInputText('Titel',
+                    getMdTitle('nl'), setMdTitle('nl')),
+                renderTextArea('Overzicht',
+                    getMdDescription('nl'), setMdDescription('nl')))),
+        renderCommon(m)
+    ));
+
+
+const renderCommon =
+    (_m: Inspire) => (
+        DIV({ className: 'app-col-wrapper meta-common' },
+            DIV({ className: 'app-col-header' }, 'FR & NL'),
+            DIV({ className: 'app-col-main' },
+                DIV({ className: 'keywords-wrapper' }, 'le truc des keywords'),
+                DIV({ className: 'responsible-person' },
+                    DIV({ className: 'label' }, tr('responsiblePerson')),
+                    INPUT({ type: 'text', value: '$UserName' })))));
+
+
+const renderAction =
+    (m: Inspire) => (
+        DIV({ className: 'meta-action' },
+            DIV({ className: 'sheet-title' }, m.id),
+            DIV({ className: 'app-col-main' },
+                DIV({ className: 'btn-validate' }, tr('save')),
+                DIV({ className: 'btn-table' },
+                    toListButton(() => appEvents.setLayout(AppLayout.List))),
+                DIV({}, 'save should display a message...'))));
+
+
+const renderEditor =
     (m: Inspire) => (
         DIV({ className: 'metadata-editor' },
             H1({}, tr('metadataEditor')),
 
             DIV({ className: 'meta-wrapper' },
-                DIV({ className: 'meta-edit' },
-                    DIV({ className: 'app-col-wrapper meta-fr' },
-                        DIV({ className: 'app-col-header' }, 'FR'),
-                        DIV({ className: 'app-col-main' },
-                            renderInputText('Titre',
-                                getMdTitle('fr'), setMdTitle('fr')),
-                            renderTextArea('Résumé',
-                                getMdDescription('fr'), setMdDescription('fr')))),
-                    DIV({ className: 'app-col-wrapper meta-nl' },
-                        DIV({ className: 'app-col-header' }, 'NL'),
-                        DIV({ className: 'app-col-main' },
-                            renderInputText('Titel',
-                                getMdTitle('nl'), setMdTitle('nl')),
-                            renderTextArea('Overzicht',
-                                getMdDescription('nl'), setMdDescription('nl')))),
-                    DIV({ className: 'app-col-wrapper meta-common' },
-                        DIV({ className: 'app-col-header' }, 'FR & NL'),
-                        DIV({ className: 'app-col-main' },
-                            DIV({ className: 'keywords-wrapper' }, 'le truc des keywords'),
-                            DIV({ className: 'responsible-person' },
-                                DIV({ className: 'label' }, tr('responsiblePerson')),
-                                INPUT({ type: 'text', value: '$UserName' }))))),
+                renderEdit(m),
+                renderAction(m))));
 
-                DIV({ className: 'meta-action' },
-                    DIV({ className: 'sheet-title' }, m.id),
-                    DIV({ className: 'app-col-main' },
-                        DIV({ className: 'btn-validate' }, tr('save')),
-                        DIV({ className: 'btn-table' }, tr('sheetList')),
-                        DIV({}, 'save should display a message...'))))));
 
 const render =
     () => (
         getCurrentDatasetMetadata()
             .fold(
             () => H1({}, `Loading Current Metadata Failed`),
-            renderForm,
+            renderEditor,
         ));
 
 
