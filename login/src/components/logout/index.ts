a95fa34333f75764c88ae860ac329218a32710d4
Pacome Beru <pacome.beru@gmail.com>
2017-10-24 14:53:59 +0200

    [login] styled logout vol2

--- login/src/components/logout/index.ts
+++ login/src/components/logout/index.ts
@@ -2,11 +2,18 @@ import { DIV, H1 } from '../elements';
 import { tryLogout } from '../../events/login';
 import button from '../button';
 import tr from '../../locale';
+import appQueries from '../../queries/app';
+import { fromNullable } from 'fp-ts/lib/Option';
+
 
 
 
 
 const logoutButton = button('logout', 'logout');
+const username = () => DIV({ className: 'username' }, fromNullable(appQueries.getUserData()).fold(
+    () => '',
+    u => u.name
+));
 
 
 const render =
@@ -14,6 +21,7 @@ const render =
         DIV({ className: 'login-wrapper' },
             H1({}, tr('connectionSDI')),
             DIV({ className: 'login-widget' },
+                username(),
                 logoutButton(tryLogout))));
 
 export default render;
