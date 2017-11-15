dab5b9e3a02b747fa39bf383b425deccddb60765
Pierre Marchand <pierremarc07@gmail.com>
2017-11-15 14:02:47 +0100

    ***

--- alias/src/components/alias/table.ts
+++ alias/src/components/alias/table.ts
@@ -1,9 +1,12 @@
 
+import * as debug from 'debug';
 // import { DIV } from 'sdi/components/elements';
 import { baseTable, SelectRowHandler } from 'sdi/components/table';
 
 import { tableAliasEvents, buildForm } from '../../events/alias';
 import { tableAliasQueries } from '../../queries/alias';
+
+const logger = debug('sdi:alias/table');
 
 const base = baseTable(
     tableAliasQueries,
@@ -14,13 +17,22 @@ const base = baseTable(
 const selectRow: SelectRowHandler =
     row => buildForm(row.from as string);
 
+// const selectCell: SelectCellHandler =
+//     (row, index) => {
+//         if (index === 4) {
+//         }
+//     }
+
 const table = base({
     className: 'xxx',
     onRowSelect: selectRow,
+    // onCellSelect: selectCell,
 });
 
 
-const render =
-    () => table();
+// const render =
+//     () => table();
 
-export default render;
+export default table;
+
+logger('loaded');
