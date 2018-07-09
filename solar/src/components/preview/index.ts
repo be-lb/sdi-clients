7f825cf5a38eea0f60ab8d1da977ca6112321f9f
pacome <pacome@atelier-cartographique.be>
2018-07-09 16:15:46 +0200

    action is noww a component

--- solar/src/components/preview/index.ts
+++ solar/src/components/preview/index.ts
@@ -2,6 +2,7 @@ import { DIV, H1 } from 'sdi/components/elements';
 import tr from 'sdi/locale';
 
 import { context } from '../context';
+import { actionSettings, actionContact, actionChange, actionPrint } from '../action';
 import { summary } from '../summary';
 
 
@@ -20,43 +21,10 @@ import { summary } from '../summary';
 const action =
     () =>
         DIV({ className: 'actions' },
-            DIV({ className: 'action-settings' },
-                H1({}, tr('solAdjustStr1')),
-                H1({}, tr('solAdjustStr2')),
-                DIV({ className: 'action-info' },
-                    DIV({}, tr('solActSettingStr1')),
-                    DIV({}, tr('solActSettingStr2')),
-                    DIV({}, tr('solActSettingStr3')),
-                    DIV({}, tr('solActSettingStr4')),
-                )),
-            DIV({ className: 'action-contact' },
-                H1({}, tr('solContactStr1')),
-                H1({}, tr('solContactStr2')),
-                DIV({ className: 'action-info' },
-                    DIV({}, tr('solActContactStr1')),
-                    DIV({}, tr('solActContactStr2')),
-                    DIV({}, tr('solActContactStr3')),
-                    DIV({}, tr('solActContactStr4')),
-                ),
-            ),
-            DIV({ className: 'action-change' },
-                H1({}, tr('solChangeStr1')),
-                H1({}, tr('solChangeStr2')),
-                DIV({ className: 'action-info' },
-                    DIV({}, tr('solActChangeStr1')),
-                    DIV({}, tr('solActChangeStr2')),
-                    DIV({}, tr('solActChangeStr3')),
-                    DIV({}, tr('solActChangeStr4')),
-                ),
-            ),
-            DIV({ className: 'action-print' },
-                H1({}, tr('solPrintStr1')),
-                H1({}, tr('solPrintStr2')),
-                DIV({ className: 'action-info' },
-                    DIV({}, tr('solActPrintStr1')),
-                    DIV({}, tr('solActPrintStr2')),
-                    DIV({}, 'PDF'),
-                )));
+            actionSettings(),
+            actionContact(),
+            actionChange(),
+            actionPrint());
 
 
 
