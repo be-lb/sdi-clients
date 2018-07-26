49b2fb1c1060d394951d694431a33813e7b4a816
Pierre Marchand <pierremarc07@gmail.com>
2018-07-26 17:30:41 +0200

    forgot file

---
+++ solar/src/components/adjust/area.ts
@@ -0,0 +1,58 @@
+import { DIV } from 'sdi/components/elements';
+import tr from 'sdi/locale';
+
+import { getOutput, totalArea } from '../../queries/simulation';
+import { setInputF } from '../../events/simulation';
+
+
+type rank = number;
+const areas = () => {
+    const ret: number[] = [];
+    for (let i = totalArea(); i >= 5; i -= 10) {
+        ret.push(i);
+    }
+    return ret.reverse();
+};
+
+
+const getArea = () => getOutput('area');// getNumInputF('pvArea');
+
+const inRange = (n: number) => {
+    const min = getArea() - 5;
+    const max = getArea() + 5;
+    return n > min && n <= max;
+};
+
+const isActive =
+    (a: number) => inRange(a);
+
+const selectItem =
+    (rank: rank) => {
+        if (isActive(rank)) {
+            return DIV({ className: `select-item  active` }, getArea().toFixed(0));
+        }
+        return DIV({
+            className: `select-item ${rank}`,
+            onClick: () => setInputF('pvArea')(rank),
+        }, rank.toFixed(0));
+    };
+
+const selectWidget =
+    () =>
+        DIV({ className: 'area-select' },
+            ...(areas().map(selectItem)),
+        );
+
+const title =
+    () => DIV({ className: 'adjust-item-title' }, `0. ${tr('usableArea')} (m2)`);
+
+
+
+export const calcArea =
+    () =>
+        DIV({ className: `adjust-item area current-${getArea()}` },
+            title(),
+            DIV({ className: 'adjust-item-widget' },
+                selectWidget(),
+            ),
+        );
