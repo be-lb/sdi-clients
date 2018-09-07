d736c3d3851549f2b34e8812aa38241886a51742
pacome <pacome@atelier-cartographique.be>
2018-09-07 16:07:04 +0200

    [solar] removed panel obstacle

--- solar/src/components/adjust/obstacle.ts
+++ solar/src/components/adjust/obstacle.ts
@@ -13,7 +13,7 @@ export interface Obstacles {
     chimneySmoke: number;
     terraceInUse: number;
     lift: number;
-    existingSolarPannel: number;
+    // existingSolarPannel: number;
 }
 
 
@@ -28,7 +28,7 @@ export const defaulObstacles =
         chimneySmoke: 0,
         terraceInUse: 0,
         lift: 0,
-        existingSolarPannel: 0,
+        // existingSolarPannel: 0,
     });
 
 const labels: { [k in Obstacle]: MessageKey } = {
@@ -39,7 +39,7 @@ const labels: { [k in Obstacle]: MessageKey } = {
     chimneySmoke: 'chimneySmoke',
     terraceInUse: 'terraceInUse',
     lift: 'lift',
-    existingSolarPannel: 'existingSolarPannel',
+    // existingSolarPannel: 'existingSolarPannel',
 };
 
 const icons: { [k in Obstacle]: string } = {
@@ -50,7 +50,7 @@ const icons: { [k in Obstacle]: string } = {
     chimneySmoke: 'icon-cheminey-smoke',
     terraceInUse: 'icon-terrace',
     lift: 'icon-lift',
-    existingSolarPannel: 'icon-panel',
+    // existingSolarPannel: 'icon-panel',
 };
 
 const obstacleItem =
@@ -89,7 +89,7 @@ const renderObstacle =
 
 export const renderGraphics =
     () => {
-        const cats: Obstacle[] = ['velux', 'dormerWindow', 'flatRoofWindow', 'chimneySmoke', 'terraceInUse', 'lift', 'existingSolarPannel'];
+        const cats: Obstacle[] = ['velux', 'dormerWindow', 'flatRoofWindow', 'chimneySmoke', 'terraceInUse', 'lift'];
 
         const elems = cats.map((cat) => {
             const n = getObstacle(cat);
@@ -118,7 +118,8 @@ export const calcObstacle =
                 renderObstacle('chimneySmoke'),
                 renderObstacle('terraceInUse'),
                 renderObstacle('lift'),
-                renderObstacle('existingSolarPannel')),
+                // renderObstacle('existingSolarPannel'),
+            ),
         );
 
 
